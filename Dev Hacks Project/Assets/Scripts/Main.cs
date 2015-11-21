7c56b947e4ccb574e14826314ec57c2fb2ebbf40
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-21 23:36:23 +0200

    Unity 3d project added

---
+++ Dev Hacks Project/Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class Main : MonoBehaviour {
+
+	private const string URL = "http://www.webv.ro/devhacks/public/upload.php";
+
+	private IEnumerator Start () {
+		WWWForm form = new WWWForm();
+		string postString = "";
+		form.AddField ("post", postString);
+		WWW www = new WWW (URL, form);
+		yield return www;
+		Debug.Log (www.text);
+	}
+}
